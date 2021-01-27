db9eafdb47a3b09d9eb1432c84902d0cfa0c2cd4
Alejandro M. Coca <alxmcr@outlook.com>
2021-01-27 00:42:47 -0400

    * Modify product object

--- src/components/forms/CreateProductForm/index.jsx
+++ src/components/forms/CreateProductForm/index.jsx
@@ -3,14 +3,12 @@ import { createProductInCollection } from 'helpers/firestore'
 
 function CreateProductForm({ rootCollection }) {
     const [name, setName] = useState("");
+    const [nextPurchase, setNextPurchase] = useState("");
 
     const createProduct = (e) => {
         e.preventDefault();
-
         const token = localStorage.getItem('tcl18-token');
-        const product = { name };
-
-        console.log("Creating a product...", product);
+        const product = { name, nextPurchase, lastPurchasedDate: null, };
         createProductInCollection(rootCollection, token, product);
     }
 
@@ -20,7 +18,42 @@ function CreateProductForm({ rootCollection }) {
             <input id="name"
                 type="text"
                 placeholder="Enter the item name"
+                autoComplete="off"
+                required="required"
+                name="item"
                 onChange={(e) => setName(e.target.value)} />
+            <br />
+            <div>
+                <label>How soon will you buy this again?</label>
+                <br />
+                <input
+                    onChange={(e) => setNextPurchase(e.target.value)}
+                    type="radio"
+                    required="required"
+                    name="nextPurchase"
+                    value={7}
+                />
+                Soon
+                <br />
+                <input
+                    onChange={(e) => setNextPurchase(e.target.value)}
+                    type="radio"
+                    required="required"
+                    name="nextPurchase"
+                    value={14}
+                />
+                Kind Of Soon
+                <br />
+                <input
+                    onChange={(e) => setNextPurchase(e.target.value)}
+                    type="radio"
+                    required="required"
+                    name="nextPurchase"
+                    value={30}
+                />
+                Not Soon
+                <br />
+            </div>
             <br />
             <input type="submit" onClick={createProduct} />
         </form>
