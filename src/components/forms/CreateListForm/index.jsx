04f20aae9ab49bb243002c0dac99ff7d07a9ec16
Alejandro M. Coca <alxmcr@outlook.com>
2021-01-26 22:23:14 -0400

    * Working Form Create List

--- src/components/forms/CreateListForm/index.jsx
+++ src/components/forms/CreateListForm/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import getToken from 'lib/tokens';
+import { useHistory } from 'react-router-dom';
 
 function CreateListForm() {
+
+    const history = useHistory();
 
     const createNewList = (e) => {
         e.preventDefault();
         console.log("Create new list...");
+        const tokenRandom = getToken();
+        // Localstorage
+        localStorage.setItem("tcl18-token", tokenRandom);
+        // Redirect
+        history.push("/list-view");
     }
 
     return (
