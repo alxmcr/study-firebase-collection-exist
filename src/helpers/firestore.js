58e47136df638c7b930d981c55e8d87f6ac76c7a
Alejandro M. Coca <alxmcr@outlook.com>
2021-01-27 00:14:29 -0400

    + createProductInCollection

--- src/helpers/firestore.js
+++ src/helpers/firestore.js
@@ -1,8 +1,23 @@
 import { db } from 'lib/firebase'
 
+const createProductInCollection = (rootCollection, token, product) => {
+    // // Add a new document in collection "cities"
+    db.collection(rootCollection).doc(token).collection("products").doc().set({
+        name: "Los Angeles",
+        state: "CA",
+        country: "USA"
+    })
+        .then(function () {
+            console.log("Document successfully written!");
+        })
+        .catch(function (error) {
+            console.error("Error writing document: ", error);
+        });
+};
+
 const existCollectionById = async (rootCollection, token) => {
     const collectionRefByToken = db.collection(rootCollection || ' ')
-                                    .where("token", "==", token || ' ');
+        .where("token", "==", token || ' ');
     try {
         const querySnapshot = await collectionRefByToken.get();
         return !querySnapshot.empty;
@@ -11,4 +26,4 @@ const existCollectionById = async (rootCollection, token) => {
     }
 };
 
-export { existCollectionById }
+export { existCollectionById, createProductInCollection }
