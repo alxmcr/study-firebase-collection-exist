bc49d4c572cc3ac37918a891acd3b110ed64322c
Alejandro M. Coca <alxmcr@outlook.com>
2021-01-26 22:22:56 -0400

    + Product Card component

---
+++ src/components/cards/ProductCard/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProductCard({ product }) {
+    return (
+        <div>
+            <p>{product.name}</p>
+        </div>
+    )
+}
+
+export default ProductCard;
