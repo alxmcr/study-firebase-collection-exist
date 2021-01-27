bcccd5bac1a3b655c4bc9fc527d2e60fb87fc1e5
Alejandro M. Coca <alxmcr@outlook.com>
2021-01-26 22:22:40 -0400

    * Change Firebase Configuration

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,12 +5,12 @@ import 'firebase/firestore';
 
 // Initalize Firebase.
 var firebaseConfig = {
-  apiKey: 'AIzaSyCUAyVKewVI4hp_1WjTka8iKzWQb1tY-ec',
-  authDomain: 'tcl-18-shopping-list.firebaseapp.com',
-  projectId: 'tcl-18-shopping-list',
-  storageBucket: 'tcl-18-shopping-list.appspot.com',
-  messagingSenderId: '1010208318256',
-  appId: '1:1010208318256:web:ed4b58b8867ec6d141def5',
+  apiKey: "AIzaSyBmfE71fh-Vv1vJk-F2WEPguZJvsBrmb0k",
+  authDomain: "react-firebase-practice-01.firebaseapp.com",
+  projectId: "react-firebase-practice-01",
+  storageBucket: "react-firebase-practice-01.appspot.com",
+  messagingSenderId: "1064525264600",
+  appId: "1:1064525264600:web:29f047f22a6de748d8c9ad"
 };
 
 let fb = firebase.initializeApp(firebaseConfig);
